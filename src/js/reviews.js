5efa9f4f35afd1a0c6f9ccddc073cfe06a6d7d28
Larysa Ivshyna <ivshynalarysa@gmail.com>
2025-08-26 00:17:34 +0100

    change reviews mobile

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,40 +1,37 @@
 import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
+import { Navigation} from "swiper/modules";
 import 'swiper/css';
 import 'swiper/css/navigation';
 
 
-
+let swiperInstance = null;
 function initSwiperReviews() {
-
-
-  const swiperReviews = new Swiper('.swiperReviews', {
+  const isMobile = window.innerWidth <= 767;
+  if (isMobile && !swiperInstance) {
+    swiperInstance = new Swiper('.swiperReviews', {
     modules: [Navigation],
-
-        slidesPerView: 1,
-      centeredSlides: false,
-      slidesPerGroupSkip: 1,
-      grabCursor: true,
-      keyboard: {
-        enabled: true,
-      },
-      breakpoints: {
-        769: {
-          slidesPerView: 2,
-          slidesPerGroup: 2,
-        },
-      },
-      scrollbar: {
-        el: ".swiper-scrollbar",
-      },
-      navigation: {
+      speed: 600,
+      spaceBetween: 30,
+      
+     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
      
       });
+  }
+  else {
+    if (!isMobile && swiperInstance) {
+      swiperInstance.destroy(true, true);
+      swiperInstance = null;
+    }
+  }
+  
     }
 
 document.addEventListener ('DOMContentLoaded', () => {
     initSwiperReviews ();
 });
+window.addEventListener('resize', () => {
+  initSwiperReviews()
+})
